10abdd2692ebcdbb6f061b9032b98b07fb3ed5f4
MehrdadFaraji <faraji.m@bayan.co.ir>
2016-08-06 13:34:07 +0430

    Create story project

---
+++ app/src/main/java/ir/approom/firstapp/MainActivity.java
@@ -0,0 +1,21 @@
+package ir.approom.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView contentTv = (TextView) findViewById(R.id.story_content_tv);
+        Button nextStoryBtn = (Button) findViewById(R.id.next_story_btn);
+
+
+
+    }
+}
